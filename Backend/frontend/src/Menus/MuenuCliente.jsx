b47114cb86c7c38751f132a99e0035b396aa3160
Dani <daniel.ahedo.999@gmail.com>
2022-04-23 19:25:34 +0200

    Comienzo desarrollo menu cliente

--- Backend/frontend/src/Menus/MuenuCliente.jsx
+++ Backend/frontend/src/Menus/MuenuCliente.jsx
@@ -12,31 +12,38 @@ function MuenuCliente(props) {
   categories = ["Ejercicios", "Entrenamientos", "Alimentacion", "Seguimiento", props.userType]
 
 
-  useEffect(()=>{
-    var loggedUser= window.localStorage.getItem('loggedUser');
-    
-    if(loggedUser == null){
+  useEffect(() => {
+    var loggedUser = window.localStorage.getItem('loggedUser');
+
+    if (loggedUser == null) {
       navigate('/login');
-    }else{
-      loggedUser= JSON.parse(loggedUser)
- 
-      if(loggedUser.Rol === 'ENTRENADOR'){
+    } else {
+      loggedUser = JSON.parse(loggedUser)
+
+      if (loggedUser.Rol === 'ENTRENADOR') {
         navigate('/menuEntrenador');
-      }if(loggedUser.Rol === 'NUTRICIONISTA'){
+      } if (loggedUser.Rol === 'NUTRICIONISTA') {
         navigate('/menuNutricionista');
       }
     }
-  
-  },[])
+
+  }, [])
 
 
   return (
     <div className="App">
       <ResponsiveAppBar loginVisivility={false} />
-      <div style={{ display: 'flex', flexDirection: 'row', height: 'calc(100% - 60px)' }}>
-        <Paper style={{ height: '100%',width:'50%' }}></Paper>
-        <Paper style={{ height: '100%',width:'50%' }}></Paper>
-
+      <div style={{ display: 'flex', flexDirection: 'row', height: 'calc(100% - 60px)', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ height: '95%', width: '90%'}}>
+          <Grid container spacing={2}  style={{ height: '100%'}}>
+            <Grid item  xs={12} md={2} style={{ height: '100%' }} >
+              <Paper elevation={3} style={{ height: '100%' }}>xs=4</Paper>
+            </Grid>
+            <Grid item xs={12} md={10}>
+              <Paper elevation={3} style={{ height: '100%' }}>xs=8</Paper>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </div>
   );
