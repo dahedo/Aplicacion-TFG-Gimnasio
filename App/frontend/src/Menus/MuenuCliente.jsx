5c254073401b38a6629c8c22f8cee73604da4532
Dani <daniel.ahedo.999@gmail.com>
2022-05-13 23:16:08 +0200

    Añadido PARQ en cliente

--- App/frontend/src/Menus/MuenuCliente.jsx
+++ App/frontend/src/Menus/MuenuCliente.jsx
@@ -1,27 +1,46 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import ResponsiveAppBar from '../navbar';
 import { useNavigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import { styled } from '@mui/material/styles';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import axios from 'axios';
 import jwt from "jwt-decode";
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 function MuenuCliente(props) {
   const navigate = useNavigate();
+
+  const [showProfilePanel, setShowProfilePanel] = useState(false);
+  const [showClientsPanel, setShowClientsPanel] = useState(false);
+  const [showDietsPanel, setShowDietsPanel] = useState(false);
+  const [createDietsPanel, setCreateDietsPanel] = useState(false);
+
+
   const [clientProfile, setClientProfile] = useState({
-    nombre:'',
+    nombre: '',
     apellidos: '',
     email: false,
-    fechaNacimiento:'',
-    username:''
-});
+    fechaNacimiento: '',
+    username: '',
+    parq1: false,
+    parq2: false,
+    parq3: false,
+    parq4: false,
+    parq5: false,
+    parq6: false,
+    parq7: false
+  });
 
 
-  
+
 
   var categories;
   categories = ["Ejercicios", "Entrenamientos", "Alimentacion", "Seguimiento", props.userType]
@@ -33,7 +52,7 @@ function MuenuCliente(props) {
     if (loggedUser == null) {
       navigate('/login');
     } else {
-      loggedUser = jwt(loggedUser) 
+      loggedUser = jwt(loggedUser)
 
       if (loggedUser.rol === 'ENTRENADOR') {
         navigate('/menuEntrenador');
@@ -41,28 +60,45 @@ function MuenuCliente(props) {
         navigate('/menuNutricionista');
       }
     }
- 
+
     const url = `http://localhost:8080/clientes/${loggedUser.userId}`
 
     var token = window.localStorage.getItem('loggedUser');
-    token =JSON.parse(token); 
+    token = JSON.parse(token);
 
     axios.get(url, { headers: { "Access-Control-Allow-Origin": "*", "Authorization": `Bearer ${token}` } }).then((response) => {
+
       setClientProfile({
-        nombre:response.data.nombre,
+        nombre: response.data.nombre,
         apellidos: response.data.apellidos,
         email: response.data.email,
-        fechaNacimiento:response.data.fechaNacimiento,
-        username: response.data.username
-    })
+        fechaNacimiento: response.data.fechaNacimiento,
+        username: response.data.username,
+        parq1: response.data.parq1 ? response.data.parq1 : false,
+        parq2: response.data.parq2 ? response.data.parq2 : false,
+        parq3: response.data.parq3 ? response.data.parq3 : false,
+        parq4: response.data.parq4 ? response.data.parq4 : false,
+        parq5: response.data.parq5 ? response.data.parq5 : false,
+        parq6: response.data.parq6 ? response.data.parq6 : false,
+        parq7: response.data.parq7 ? response.data.parq7 : false
+
+      })
     }, (error) => {
       console.log(error);
     });
 
-
-
   }, [])
 
+  const showProfile = async (e) => {
+    e.preventDefault();
+
+    console.log(clientProfile)
+
+    setShowProfilePanel(true)
+    setShowClientsPanel(false)
+    setShowDietsPanel(false)
+    setCreateDietsPanel(false)
+  }
 
   const submit = async (e) => {
     e.preventDefault();
@@ -75,27 +111,88 @@ function MuenuCliente(props) {
         <div style={{ height: '95%', width: '90%' }}>
           <Grid container spacing={2} style={{ height: '100%' }}>
             <Grid item xs={12} md={2} style={{ height: '100%' }} >
-              <Paper elevation={3} style={{ height: '100%', display:'flex', flexDirection:'column', alignItems: 'center'  }}>
+              <Paper elevation={3} style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Avatar
                   alt="Remy Sharp"
-                  src= "/static/images/avatar/1.jpg" 
+                  src="/static/images/avatar/1.jpg"
                   sx={{ width: 150, height: 150 }}
-                  style={{marginTop:'30px'}}
+                  style={{ marginTop: '30px' }}
                 />
                 <p>Bienvenido {clientProfile.nombre}</p>
-                <Button variant="contained" onClick={submit} style={{width:'90%', marginTop:'30px'}}>Perfil</Button>
-                <Button variant="contained" onClick={submit} style={{width:'90%', marginTop:'100px'}}>Dietas</Button>
-                <Button variant="contained" onClick={submit} style={{width:'90%', marginTop:'30px'}}>Entrenamientos</Button>
-                <Button variant="contained" onClick={submit} style={{width:'90%', marginTop:'30px'}}>Ejercicios</Button>
+                <Button variant="contained" onClick={showProfile} style={{ width: '90%', marginTop: '30px' }}>Perfil</Button>
+                <Button variant="contained" onClick={submit} style={{ width: '90%', marginTop: '100px' }}>Dietas</Button>
+                <Button variant="contained" onClick={submit} style={{ width: '90%', marginTop: '30px' }}>Entrenamientos</Button>
+                <Button variant="contained" onClick={submit} style={{ width: '90%', marginTop: '30px' }}>Ejercicios</Button>
 
 
               </Paper>
             </Grid>
 
             <Grid item xs={12} md={10}>
-              <Paper elevation={3} style={{ height: '100%' }}>
+              {showProfilePanel ?
 
-              </Paper>
+                <React.Fragment>
+                  <Paper elevation={3}>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Nombre" variant="outlined" defaultValue={clientProfile.nombre}> </TextField>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Apellidos" variant="outlined" defaultValue={clientProfile.apellidos}> </TextField>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Email" variant="outlined" defaultValue={clientProfile.email}></TextField>
+                  </Paper>
+                  <Paper style={{ marginTop: '15px' }} elevation={3}>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Edad" variant="outlined"> </TextField>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Peso" variant="outlined"> </TextField>
+                    <TextField style={{ margin: '15px 15px 15px 15px' }} id="outlined-basic" label="Altura" variant="outlined"></TextField>
+                  </Paper>
+                  <Paper elevation={3}>
+                    <Table>
+                      <TableBody>
+                        <TableRow style={{maxHeight:'30px !important'}}>
+                          <TableCell>¿Alguna vez le ha diagnosticado su médico un problema en el corazón, recomendándole que solo haga deporte bajo supervisión médica?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq1 ? "contained" : null}  >Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq1 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>¿Siente dolor en el pecho al hacer ejercicio?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq2 ? "contained" : null} >Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq2 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>¿Ha tenido dolor en el pecho durante el último mes?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq3 ? "contained" : null}>Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq3 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>¿Se marea frecuentemente o ha perdido el conocimiento demasiadas veces?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq4 ? "contained" : null}>Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq4 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>¿Toma medicación para la presión arterial u otro problema circulatorio?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq5 ? "contained" : null}>Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq5 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>¿Tiene problemas en las articulaciones o algún dolor que se agrava haciendo ejercicio?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq6 ? "contained" : null}>Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq6 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+                        
+                        <TableRow>
+                          <TableCell>¿Cuenta con alguna otra recomendación médica que le recomiende no hacer deporte con mucha intensidad?</TableCell>
+                          <TableCell><Button variant={clientProfile.parq7 ? "contained" : null}>Si</Button></TableCell>
+                          <TableCell><Button variant={clientProfile.parq7 ? null:"contained"}>No</Button></TableCell>
+                        </TableRow>
+
+                      </TableBody>
+                    </Table>
+  
+                  </Paper>
+                </React.Fragment>
+                : null}
             </Grid>
           </Grid>
         </div>
--- App/src/main/java/com/mygym/model/Usuarios/Cliente.java
+++ App/src/main/java/com/mygym/model/Usuarios/Cliente.java
@@ -27,6 +27,21 @@ public class Cliente extends Usuario {
 	@Column
 	private String fechaNacimiento;
 
+	@Column
+	private Boolean parq1;
+	@Column
+	private Boolean parq2;
+	@Column
+	private Boolean parq3;
+	@Column
+	private Boolean parq4;
+	@Column
+	private Boolean parq5;
+	@Column
+	private Boolean parq6;
+	@Column
+	private Boolean parq7;
+
 	@ManyToOne
 	@JoinColumn(name = "entrenador_id", nullable = true)
 	private Entrenador entrenador;
@@ -123,6 +138,104 @@ public class Cliente extends Usuario {
 	 */
 	public void setEntrenamientosClientes(Set<EntrenamientoCliente> entrenamientosClientes) {
 		this.entrenamientosClientes = entrenamientosClientes;
+	}
+
+	/**
+	 * @return the parq1
+	 */
+	public Boolean getParq1() {
+		return parq1;
+	}
+
+	/**
+	 * @param parq1 the parq1 to set
+	 */
+	public void setParq1(Boolean parq1) {
+		this.parq1 = parq1;
+	}
+
+	/**
+	 * @return the parq2
+	 */
+	public Boolean getParq2() {
+		return parq2;
+	}
+
+	/**
+	 * @param parq2 the parq2 to set
+	 */
+	public void setParq2(Boolean parq2) {
+		this.parq2 = parq2;
+	}
+
+	/**
+	 * @return the parq3
+	 */
+	public Boolean getParq3() {
+		return parq3;
+	}
+
+	/**
+	 * @param parq3 the parq3 to set
+	 */
+	public void setParq3(Boolean parq3) {
+		this.parq3 = parq3;
+	}
+
+	/**
+	 * @return the parq4
+	 */
+	public Boolean getParq4() {
+		return parq4;
+	}
+
+	/**
+	 * @param parq4 the parq4 to set
+	 */
+	public void setParq4(Boolean parq4) {
+		this.parq4 = parq4;
+	}
+
+	/**
+	 * @return the parq5
+	 */
+	public Boolean getParq5() {
+		return parq5;
+	}
+
+	/**
+	 * @param parq5 the parq5 to set
+	 */
+	public void setParq5(Boolean parq5) {
+		this.parq5 = parq5;
+	}
+
+	/**
+	 * @return the parq6
+	 */
+	public Boolean getParq6() {
+		return parq6;
+	}
+
+	/**
+	 * @param parq6 the parq6 to set
+	 */
+	public void setParq6(Boolean parq6) {
+		this.parq6 = parq6;
+	}
+
+	/**
+	 * @return the parq7
+	 */
+	public Boolean getParq7() {
+		return parq7;
+	}
+
+	/**
+	 * @param parq7 the parq7 to set
+	 */
+	public void setParq7(Boolean parq7) {
+		this.parq7 = parq7;
 	}
 
 }
