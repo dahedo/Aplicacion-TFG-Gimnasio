1c0eee3a90138ebed379061da6fc3ec8a201cd83
Dani <daniel.ahedo.999@gmail.com>
2022-06-01 22:37:45 +0200

    Avances en dietas semanales

--- App/frontend/src/Menus/NutritionistCreateWeeklyDiet.jsx
+++ App/frontend/src/Menus/NutritionistCreateWeeklyDiet.jsx
@@ -1,6 +1,9 @@
 import {
+  Box,
   Button,
+  Collapse,
   Dialog,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -9,27 +12,38 @@ import {
   TableRow,
   TextField,
 } from "@mui/material";
-
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
 import React, { useState } from "react";
 
 function NutritionistCreateWeeklyDiet(props) {
-  console.log(props.dailyDietList);
+  const semana = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo",
+  ];
   const [nombreDieta, setNombreDieta] = useState("");
   const [caloriasDieta, setCaloriasDieta] = useState("");
   const [alergiasDieta, setAlergiasDieta] = useState("");
   const [otrosDieta, setOtrosDieta] = useState("");
 
   const [dietaSemanal, setDietaSemanal] = useState({
-    lunes: {},
-    martes: {},
-    miercoles: {},
-    jueves: {},
-    viernes: {},
-    sabado: {},
-    domingo: {},
+    Lunes: {},
+    Martes: {},
+    Miercoles: {},
+    Jueves: {},
+    Viernes: {},
+    Sabado: {},
+    Domingo: {},
   });
   const [enableBusqueda, setEnableBusqueda] = useState(false);
+  const [diaBusqueda, setDiaBusqueda] = useState("");
 
   const changeName = async (e) => {
     e.preventDefault();
@@ -51,13 +65,21 @@ function NutritionistCreateWeeklyDiet(props) {
     }
   };
 
+  const seleccionarDieta = async (row, e) => {
+    console.log(diaBusqueda);
+    setDietaSemanal({ ...dietaSemanal, [diaBusqueda]: row });
+    console.log(dietaSemanal);
+  };
+
   const buscar = async (e) => {
     e.preventDefault();
+    setDiaBusqueda(e.target.id);
     setEnableBusqueda(true);
   };
   const handleClose = async (e) => {
     e.preventDefault();
     setEnableBusqueda(false);
+    setDiaBusqueda("");
   };
 
   return (
@@ -65,7 +87,7 @@ function NutritionistCreateWeeklyDiet(props) {
       <Paper
         elevation={3}
         style={{
-          height: "12%",
+          height: "80px",
           display: "flex",
           flexDirection: "column",
           justifyContent: "space-between",
@@ -121,77 +143,25 @@ function NutritionistCreateWeeklyDiet(props) {
         style={{
           display: "flex",
           flexDirection: "column",
-          height: "65%",
           padding: "20px 20px 20px 20px",
           marginTop: "10px",
+          height: "410px",
         }}
       >
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableBody>
-              <TableRow>
-                <TableCell>Lunes</TableCell>
-                <TableCell>
-                  <Button onClick={buscar} id="Lunes">
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Martes</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Miercoles</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Jueves</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Viernes</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Sabado</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Domingo</TableCell>
-                <TableCell>
-                  <Button>
-                    <SearchIcon fontSize="small" />
-                    {"Buscar"}
-                  </Button>
-                </TableCell>
-              </TableRow>
+              {semana.map((dia) => (
+                <TableRow key={dia}>
+                  <TableCell>{dia}</TableCell>
+                  <TableCell>
+                    <Button onClick={buscar} id={dia}>
+                      <SearchIcon fontSize="small" />
+                      {"Buscar"}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
           <div>
@@ -210,15 +180,84 @@ function NutritionistCreateWeeklyDiet(props) {
         </TableContainer>
       </Paper>
 
-      <Dialog onClose={handleClose} open={enableBusqueda}>
-        <TableContainer component={Paper}>
-          <TableBody>
-            <TableRow>z</TableRow>
-          </TableBody>
-        </TableContainer>
+      <Dialog
+        fullWidth={true}
+        maxWidth={"lg"}
+        onClose={handleClose}
+        open={enableBusqueda}
+      >
+        <div style={{ height: "600px", padding: "20px 20px 20px 20px" }}>
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableBody>
+                {props.dailyDietList.map((row) => (
+                  <Row
+                    seleccionarDieta={seleccionarDieta}
+                    key={row.name}
+                    row={row}
+                  />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </Dialog>
     </>
   );
 }
 
 export default NutritionistCreateWeeklyDiet;
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.nombre}
+        </TableCell>
+        <TableCell
+          onClick={(e) => props.seleccionarDieta(row, e)}
+          component="th"
+          scope="row"
+        >
+          <AddIcon />
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Table size="small" aria-label="purchases">
+                <TableBody>
+                  {Object.entries(row).map(([key, value]) =>
+                    key !== "nombre" &&
+                    key !== "id" &&
+                    value !== "" &&
+                    value !== null ? (
+                      <TableRow key={key}>
+                        <TableCell>{key}</TableCell>
+                        <TableCell>{value}</TableCell>
+                      </TableRow>
+                    ) : null
+                  )}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
