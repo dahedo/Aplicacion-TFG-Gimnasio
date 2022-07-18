1e1aca263f09856a9ad91ee0ca3f0da028196963
Dani <daniel.ahedo.999@gmail.com>
2022-07-18 12:51:24 +0200

    LandingPage imagen CC0 y cambiado estilo

--- App/frontend/src/LandingPage.jsx
+++ App/frontend/src/LandingPage.jsx
@@ -1,21 +1,38 @@
 import ResponsiveAppBar from "./navbar";
-import logo from "./logo.svg";
+import logo from "./Assets/LandingPageIcon.svg";
 
 const LandingPage = () => {
   return (
-    <div style={{ height: "100%" }}>
+    <div className="App">
       <ResponsiveAppBar loginVisivility={true} logoutVisivility={false} />
       <div
         style={{
-          height: "calc(100% - 60px)",
+          height: "calc(100vh - 70px)",
           display: "flex",
           flexDirection: "row",
         }}
       >
-        <div style={{ width: "50%" }}>
-          <h1> ¡Bienvenido a tu web de entrenamiento personal!</h1>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <h1
+            style={{
+              alignSelf: "center",
+              fontSize: "90px",
+              fontWeight: "300",
+            }}
+          >
+            ¡Bienvenido
+            <br /> a tu web <br />
+            de entrenamiento
+            <br /> personal!
+          </h1>
         </div>
-        <div style={{ display: "flex", width: "50%" }}>
+        <div style={{ width: "50%", alignSelf: "center" }}>
           <img src={logo} alt="" style={{ width: "100%" }} />
         </div>
       </div>
